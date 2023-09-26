9c9f29ba66a9fc2d9c056d4ceadf181acc681574
Facundo Velazco <facundovelazco13@gmail.com>
2023-09-26 10:30:05 -0300

    nada

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import keras
 import utils
 import tradingBot
 
-path = 'nasdaq-index-original.json'
+path = 'nasdaq-index-365.json'
 
 raw_data = utils.getData(path)
 
@@ -12,7 +12,8 @@ min_open = min(item["open"] for item in raw_data)
 
 normalized_data = utils.normalize(raw_data, max_open, min_open)
 
-((train_data_4days, train_labels_4days), (validation_data_4days, validation_labels_4days),(test_data_4days, test_labels_4days)) = utils.genTrainData9DaysBf(normalized_data)
+((train_data_4days, train_labels_4days), (validation_data_4days, validation_labels_4days),
+ (test_data_4days, test_labels_4days)) = utils.genTrainData4DaysBf(normalized_data)
 model = utils.build_model_regression(len(train_data_4days[0]))
 
 print("Comenzando entrenamiento...")
@@ -30,6 +31,6 @@ predicted_values = utils.desnormalizeList(predicted_values, max_open, min_open)
 real_values = utils.desnormalize(test_labels_4days, max_open, min_open)
 
 simu_balance = tradingBot.simulation(real_values,predicted_values)
-plt.plot(simu_balance)
-plt.show()
+utils.plotResults([simu_balance])
+utils.plotResults([predicted_values])
 utils.plotResults([real_values,predicted_values])
